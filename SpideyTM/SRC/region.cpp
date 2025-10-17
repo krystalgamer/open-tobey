8b589b63e3407b9cca2489ba6c7f6f3dfbf56bdb
krystalgamer <callofdutypsp@gmail.com>
2025-10-18 00:10:41 +0200

    stub: region::remove_local_thread

--- SpideyTM/SRC/region.cpp
+++ SpideyTM/SRC/region.cpp
@@ -1,6 +1,10 @@
 #include "region.h"
 
+typedef void (__fastcall *remove_from_local_thread_ptr)(region*, int, vm_thread*);
+
 // @TODO
 void region::remove_local_thread(vm_thread * thr)
 {
+	remove_from_local_thread_ptr remove_from_local_thread_f = (remove_from_local_thread_ptr)0x0050F8B0;
+	remove_from_local_thread_f(this, 0, thr);
 }
