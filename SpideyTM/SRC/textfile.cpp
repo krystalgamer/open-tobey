0b35a881c641f7d0fe72c2197c7d32f4eaa1a4b5
krystalgamer <callofdutypsp@gmail.com>
2025-10-27 19:15:53 +0100

    patch: text_file::close

--- SpideyTM/SRC/textfile.cpp
+++ SpideyTM/SRC/textfile.cpp
@@ -723,4 +723,6 @@ void patch_text_file(void)
 {
 	PATCH_PUSH_RET_POLY(0x007A49C0, text_file::text_file, "??0text_file@@QAE@XZ");
 	PATCH_PUSH_RET_POLY(0x007A4A00, text_file::~text_file, "??1text_file@@QAE@XZ");
+
+	PATCH_PUSH_RET(0x00007A4B20, text_file::close);
 }
